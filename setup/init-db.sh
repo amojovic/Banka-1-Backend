cc4a1ac83eb90e2028bae0c44cea538612d2c24c
subin <hpro444@gmail.com>
2026-05-10 23:53:10 +0200

    Rev

--- setup/init-db.sh
+++ setup/init-db.sh
@@ -16,5 +16,4 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-E
     CREATE DATABASE orderdb;
     CREATE DATABASE stock_db;
     CREATE DATABASE credit_db;
-    CREATE DATABASE investment_fund_db;
 EOSQL
