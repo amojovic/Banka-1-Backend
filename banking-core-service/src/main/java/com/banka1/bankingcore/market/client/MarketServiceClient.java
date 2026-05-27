745ee639a7fce77320bb3fbec85d8cfabe553c39
IgorGajic <igajic7823rn@raf.rs>
2026-05-27 23:19:45 +0200

    popravio: celu otc trgovinu, kupovinu hartija, market servis koji nije hteo da se digne, ostalo je da se popravi ivesticija (uplata) na fond koliko ja vidim

--- banking-core-service/src/main/java/com/banka1/bankingcore/market/client/MarketServiceClient.java
+++ banking-core-service/src/main/java/com/banka1/bankingcore/market/client/MarketServiceClient.java
@@ -2,7 +2,6 @@ package com.banka1.bankingcore.market.client;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClient;
 
@@ -10,7 +9,6 @@ import java.math.BigDecimal;
 
 @Slf4j
 @Component
-@Profile("!local")
 @RequiredArgsConstructor
 public class MarketServiceClient {
 
